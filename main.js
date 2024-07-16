a1efb99b3cb7890fb38dd5df9bd0b394a3284fcb
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2024-07-16 12:24:12 +0200

    dd

---
+++ main.js
@@ -0,0 +1,3 @@
+import './src/js/menuMb';
+import './src/js/isActive';
+import '../node_modules/modern-normalize';
--- src/main.js
+++
@@ -1,3 +0,0 @@
-import './js/menuMb';
-import './js/isActive';
-import '../node_modules/modern-normalize';
